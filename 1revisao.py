76a5237b5e1bca610e71046ae8bba9855ca7a7b3
VanessAzevedo <vanessajob7@gmail.com>
2024-11-04 11:32:41 -0300

    Add files via upload

---
+++ 1revisao.py
@@ -0,0 +1,24 @@
+def calcularDivisao(v1, v2):
+    resultado = v1/v2
+    return resultado
+
+print('Revisão de Python') #print é uma função
+
+print(325 * 5)
+
+total = 325 * 5
+
+print(total)
+
+valor = input('Digite alguma coisa: ')
+
+for i in range(10):
+    print('Foi digitado:',valor)
+    print(f'Foi digitado: {valor}')
+
+print(calcularDivisao(5, 1))
+
+lista = [5, 2, 10, 25]
+dicionario = {"Representatne"}
+
+
